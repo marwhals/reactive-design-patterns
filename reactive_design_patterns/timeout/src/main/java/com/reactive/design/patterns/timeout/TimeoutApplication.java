da4661e3f75d1ec7b2234da148d6c566c1824dda
marwhals <marjan.mubarok2012@gmail.com>
2025-08-21 14:46:10 +0100

    Setup timeout service

---
+++ reactive_design_patterns/timeout/src/main/java/com/reactive/design/patterns/timeout/TimeoutApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.design.patterns.timeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeoutApplication.class, args);
+	}
+
+}
---
+++ reactive_design_patterns/timeout/src/test/java/com/reactive/design/patterns/timeout/TimeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.design.patterns.timeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
