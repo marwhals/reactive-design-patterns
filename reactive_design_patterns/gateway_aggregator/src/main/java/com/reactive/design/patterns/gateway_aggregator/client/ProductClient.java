2e04f069105a2de63431532df53de521dd94558a
marwhals <marjan.mubarok2012@gmail.com>
2025-08-21 13:03:51 +0100

    Add external service clients

---
+++ reactive_design_patterns/gateway_aggregator/src/main/java/com/reactive/design/patterns/gateway_aggregator/client/ProductClient.java
@@ -0,0 +1,31 @@
+package com.reactive.design.patterns.gateway_aggregator.client;
+
+import com.reactive.design.patterns.gateway_aggregator.dto.ProductResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductClient {
+
+    private final WebClient client;
+
+    public ProductClient(@Value("${sec01.product.service}") String baseUrl){
+        this.client = WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+    public Mono<ProductResponse> getProduct(Integer id){
+        return this.client
+                .get()
+                .uri("{id}", id)
+                .retrieve()
+                .bodyToMono(ProductResponse.class)
+                .onErrorResume(ex -> Mono.empty());
+    }
+
+
+}
