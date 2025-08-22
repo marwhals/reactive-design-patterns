9ca6a6210ad19478da05cdde4331825dfe512bb1
marwhals <marjan.mubarok2012@gmail.com>
2025-08-22 18:06:21 +0100

    Add payment handlers

---
+++ reactive_design_patterns/orchestrator_parallel/src/main/java/com/reactive/design/patterns/orchestrator_parallel/service/PaymentOrchestrator.java
@@ -0,0 +1,42 @@
+package com.reactive.design.patterns.orchestrator_parallel.service;
+
+import com.reactive.design.patterns.orchestrator_parallel.client.UserClient;
+import com.reactive.design.patterns.orchestrator_parallel.dto.OrchestrationRequestContext;
+import com.reactive.design.patterns.orchestrator_parallel.dto.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+@Service
+public class PaymentOrchestrator extends Orchestrator {
+
+    @Autowired
+    private UserClient client;
+
+    @Override
+    public Mono<OrchestrationRequestContext> create(OrchestrationRequestContext orchestrationRequestContext) {
+        return this.client.deduct(orchestrationRequestContext.getPaymentRequest())
+                .doOnNext(orchestrationRequestContext::setPaymentResponse)
+                .thenReturn(orchestrationRequestContext);
+    }
+
+    @Override
+    public Predicate<OrchestrationRequestContext> isSuccess() {
+        return orchestrationRequestContext
+                -> Status.SUCCESS.equals(orchestrationRequestContext.getPaymentResponse().getStatus());
+    }
+
+    @Override
+    public Consumer<OrchestrationRequestContext> cancel() {
+        return orchestrationRequestContext
+                -> Mono.just(orchestrationRequestContext)
+                .filter(isSuccess())
+                .map(OrchestrationRequestContext::getPaymentRequest)
+                .flatMap(this.client::refund)
+                .subscribe();
+    }
+
+}
