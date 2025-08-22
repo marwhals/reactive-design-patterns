2a14bec782602d39aa45bced217fd62ea1d5b416
marwhals <marjan.mubarok2012@gmail.com>
2025-08-22 20:34:34 +0100

    Setup project

---
+++ reactive_design_patterns/splitter/notes.md
@@ -0,0 +1,4 @@
+# Splitter pattern notes
+
+---
+
--- reactive_design_patterns/splitter/src/main/java/com/reactive/design/patterns/splitter/SplitterApplication.java
+++ reactive_design_patterns/splitter/src/main/java/com/reactive/design/patterns/splitter/SplitterApplication.java
@@ -3,7 +3,7 @@ package com.reactive.design.patterns.splitter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.reactive.design.patterns.splitter")
 public class SplitterApplication {
 
 	public static void main(String[] args) {
