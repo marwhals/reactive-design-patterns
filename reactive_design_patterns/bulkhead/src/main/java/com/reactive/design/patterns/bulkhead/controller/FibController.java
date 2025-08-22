8a86c5c503294c2ee548a1522489ddeecd592e24
marwhals <marjan.mubarok2012@gmail.com>
2025-08-22 22:10:21 +0100

    Add tests

--- reactive_design_patterns/bulkhead/src/main/java/com/reactive/design/patterns/bulkhead/controller/FibController.java
+++ reactive_design_patterns/bulkhead/src/main/java/com/reactive/design/patterns/bulkhead/controller/FibController.java
@@ -1,7 +1,5 @@
 package com.reactive.design.patterns.bulkhead.controller;
 
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
