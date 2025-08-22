cd04d28c0b173f43c9f55f9de2b1661d36a212c8
marwhals <marjan.mubarok2012@gmail.com>
2025-08-22 19:36:21 +0100

    Edit util class

--- reactive_design_patterns/orchestrator_sequential/src/main/java/com/reactive/design/patterns/orchestrator_sequential/util/OrchestrationUtil.java
+++ reactive_design_patterns/orchestrator_sequential/src/main/java/com/reactive/design/patterns/orchestrator_sequential/util/OrchestrationUtil.java
@@ -1,19 +1,14 @@
 package com.reactive.design.patterns.orchestrator_sequential.util;
 
-import com.reactive.design.patterns.orchestrator_parallel.dto.InventoryRequest;
-import com.reactive.design.patterns.orchestrator_parallel.dto.OrchestrationRequestContext;
-import com.reactive.design.patterns.orchestrator_parallel.dto.PaymentRequest;
-import com.reactive.design.patterns.orchestrator_parallel.dto.ShippingRequest;
+
+import com.reactive.design.patterns.orchestrator_sequential.dto.InventoryRequest;
+import com.reactive.design.patterns.orchestrator_sequential.dto.OrchestrationRequestContext;
+import com.reactive.design.patterns.orchestrator_sequential.dto.PaymentRequest;
+import com.reactive.design.patterns.orchestrator_sequential.dto.ShippingRequest;
 
 public class OrchestrationUtil {
 
-    public static void buildRequestContext(OrchestrationRequestContext orchestrationRequestContext){
-        buildPaymentRequest(orchestrationRequestContext);
-        buildInventoryRequest(orchestrationRequestContext);
-        buildShippingRequest(orchestrationRequestContext);
-    }
-
-    private static void buildPaymentRequest(OrchestrationRequestContext orchestrationRequestContext){
+    public static void buildPaymentRequest(OrchestrationRequestContext orchestrationRequestContext){
         var paymentRequest = PaymentRequest.create(
                 orchestrationRequestContext.getOrderRequest().getUserId(),
                 orchestrationRequestContext.getProductPrice() * orchestrationRequestContext.getOrderRequest().getQuantity(),
@@ -22,20 +17,20 @@ public class OrchestrationUtil {
         orchestrationRequestContext.setPaymentRequest(paymentRequest);
     }
 
-    private static void buildInventoryRequest(OrchestrationRequestContext orchestrationRequestContext){
+    public static void buildInventoryRequest(OrchestrationRequestContext orchestrationRequestContext){
         var inventoryRequest = InventoryRequest.create(
-                orchestrationRequestContext.getOrderId(),
+                orchestrationRequestContext.getPaymentResponse().getPaymentId(),
                 orchestrationRequestContext.getOrderRequest().getProductId(),
                 orchestrationRequestContext.getOrderRequest().getQuantity()
         );
         orchestrationRequestContext.setInventoryRequest(inventoryRequest);
     }
 
-    private static void buildShippingRequest(OrchestrationRequestContext orchestrationRequestContext){
+    public static void buildShippingRequest(OrchestrationRequestContext orchestrationRequestContext){
         var shippingRequest = ShippingRequest.create(
                 orchestrationRequestContext.getOrderRequest().getQuantity(),
                 orchestrationRequestContext.getOrderRequest().getUserId(),
-                orchestrationRequestContext.getOrderId()
+                orchestrationRequestContext.getInventoryResponse().getInventoryId()
         );
         orchestrationRequestContext.setShippingRequest(shippingRequest);
     }
